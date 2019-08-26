5e40ac5c1bbf180978cd94d92a3a88b0835791db
diiceas <forthepost@gmail.com>
2019-08-26 09:03:36 +0300

    implement login component minor refactoring

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,28 @@
-import { UserService } from "./../user/user.service";
-import { Component, Output, EventEmitter } from "@angular/core";
+import { UserService } from './../user/user.service';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   @Output() sendCredentials: EventEmitter<any> = new EventEmitter();
 
   private login: string;
   private password: string;
-  private isLoginFormValid: boolean = false;
-  private isCredentialsValid: boolean = true;
-  private _userService: UserService;
+  // A declare boolean with a shortucut
+  // ref: Type boolean trivially inferred from a boolean literal, remove type annotation (no-inferrable-types)
+  private isLoginFormValid = false;
+  private isCredentialsValid = true;
+  // B.1 do not use Underscores to name variables
+  // B.2 no need to declare private variables for services
+  // private _userService: UserService;
   private userID: string;
 
-  constructor(userService: UserService) {
-    this._userService = userService;
-  }
+  // B.3 Typescript taking care of creating a local variables
+  // B.4 Use 'private' accessor injected services
+  constructor(private userService: UserService) {}
 
   onLoginChange(login: string) {
     this.login = login;
@@ -35,7 +39,8 @@ export class LoginComponent {
   }
 
   onSubmit() {
-    this._userService.getUserID(this.login, this.password).subscribe(data => {
+    // B.4 Using typescript generated local variable
+    this.userService.getUserID(this.login, this.password).subscribe(data => {
       data.forEach(id => {
         this.userID = id.id;
       });
