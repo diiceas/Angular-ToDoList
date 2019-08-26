d85e8f2c929d3a83dc40c7b1c5a2ba2553e41547
diiceas <forthepost@gmail.com>
2019-08-26 09:02:49 +0300

    use single quotes via prettier (you can setup this feature in the settings)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, OnInit } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { AutofocusModule } from "angular-autofocus-fix";
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AutofocusModule } from 'angular-autofocus-fix';
 
-import { AppComponent } from "./app.component";
-import { environment } from "src/environments/environment";
-import { TasksComponent } from "./tasks/tasks.component";
-import { RegisterComponent } from "./register/register.component";
-import { LoginComponent } from "./login/login.component";
-import { UserService } from "./user/user.service";
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { TasksComponent } from './tasks/tasks.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { UserService } from './user/user.service';
 
-const routes = [{ path: "", component: AppComponent }, { path: "tasks/:userID", component: TasksComponent }];
+const routes = [
+  { path: '', component: AppComponent },
+  { path: 'tasks/:userID', component: TasksComponent }
+];
 
 @NgModule({
   declarations: [AppComponent, TasksComponent, RegisterComponent, LoginComponent],
